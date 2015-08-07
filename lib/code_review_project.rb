a98eaf8b4ee13a6da1b5cc25661a5c67910284a1
Trevor Scherling <scherling.trevor@gmail.com>
2015-08-07 13:57:55 -0700

    seeing if ths works.

---
+++ lib/code_review_project.rb
@@ -0,0 +1,26 @@
+class Phrase
+  attr_accessor :words
+
+  def initialize(words)
+    @words = words
+  end
+
+def word_count
+  word_list = {}
+  word.split(" ").each do |word|
+    word_list = process_word_in_list(word, word_list)
+  end
+  word_list
+    # Split string based on one or more whitespace characters.
+    # ... Then return the length of the array.
+    value.split(/\s+/).length
+end
+
+value = "To be or not to be, that is the question."
+puts wordcount(value)
+
+value = "Stately, plump Buck Mulligan came from the stairhead"
+puts wordcount(value)
+
+puts wordcount " "
+puts wordcount ""
